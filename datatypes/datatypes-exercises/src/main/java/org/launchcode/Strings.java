dc2439988509e0d6a2d7ef42712f034cc8beca98
mpres08 <158608642+mpres08@users.noreply.github.com>
2024-09-06 12:36:13 -0500

    finished exercise

--- datatypes/datatypes-exercises/src/main/java/org/launchcode/Strings.java
+++ datatypes/datatypes-exercises/src/main/java/org/launchcode/Strings.java
@@ -1,4 +1,20 @@
 package org.launchcode;
+import java.util.Scanner;
 
 public class Strings {
+    public static void main(String[] args) {
+       String alice = "Alice was beginning to get very tired of sitting by her sister on the bank, and of having nothing to do: once or twice she had peeped into the book her sister was reading, but it had no pictures or conversations in it, ‘and what is the use of a book,’ thought Alice ‘without pictures or conversation?'".toLowerCase();
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Search for a term: ");
+        String searchTerm = input.nextLine().toLowerCase();
+
+        if (alice.contains(searchTerm)) {
+            System.out.println("true");
+            System.out.println(alice.indexOf(searchTerm));
+        } else {
+            System.out.print("false");
+        }
+    }
 }
