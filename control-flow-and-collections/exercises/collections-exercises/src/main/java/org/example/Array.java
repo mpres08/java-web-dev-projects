dbb3e2424638a3f8439a91a60b6950aa550d3b53
mpres08 <158608642+mpres08@users.noreply.github.com>
2024-09-09 20:36:34 -0500

    upload exercise

--- control-flow-and-collections/exercises/collections-exercises/src/main/java/org/example/Array.java
+++ control-flow-and-collections/exercises/collections-exercises/src/main/java/org/example/Array.java
@@ -1,4 +1,21 @@
 package org.example;
 
+import java.util.Arrays;
+
 public class Array {
+    public static void main(String[] args) {
+        int[] intArray = {1, 1, 2, 3, 5, 8};
+        for (int i : intArray) {
+            if (i % 2 !=0) {
+                System.out.println(i);
+            }
+        }
+        String eggs = "I would not, could not, in a box. I would not, could not with a fox. I will not eat them in a " +
+                "house. I will not eat them with a mouse.";
+        String[] words = eggs.split(" ");
+        System.out.println(Arrays.toString(words));
+
+        String[] sentences = eggs.split("\\.");
+        System.out.println(Arrays.toString(sentences));
+    }
 }
