5a317f1bf9822e6ffe847eed68a2a326669ddf1a
mpres08 <158608642+mpres08@users.noreply.github.com>
2024-09-12 13:17:19 -0500

    upload exercise classes

---
+++ classes-part-one/exercises/src/main/java/Course.java
@@ -0,0 +1,7 @@
+import java.util.ArrayList;
+
+public class Course {
+    private String name;
+    private String courseName;
+    private ArrayList<Student> courseRoster;
+}
---
+++ classes-part-one/exercises/src/main/java/Teacher.java
@@ -0,0 +1,6 @@
+public class Teacher {
+    String firstName;
+    String lastName;
+    String subject;
+    Integer yearsTeaching;
+}
